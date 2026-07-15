ca2af0c713c67d7a3869faecf56b22b749968e08
Ritesh Kumar Verma <ritesh782002@gmail.com>
2026-07-15 21:08:59 +0530

    updated the project added ui

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-import sys
 
 
 
@@ -9,14 +8,14 @@ import sys
 CLASS_NAMES = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
 
 
-
-
-def main():
-    
+def load_model():
     model = tf.keras.models.load_model("model/plant_disease_model.h5")
     
-    print("Hello from plant-disease-detection-system!")
-    img = Image.open("Test_Sample/image.png")
+
+
+def predict(path):
+    
+    img = Image.open(path)
     img = img.resize((224,224))
     
     if img.mode != "RGB":
@@ -43,36 +42,10 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    path =  "G:\AI\Plant_Disease_Detection_System\Test_Sample\l1.jpg"
+    predict(path)
 
 
 
 
 
-
-
-# import numpy as np
-# from tensorflow.keras.preprocessing import image
-
-# def predict_plant_disease(img_path, model, train_generator):
-#     # Load and preprocess image
-#     img = image.load_img(img_path, target_size=(224, 224))
-#     img_array = image.img_to_array(img) / 255.0
-#     img_array = np.expand_dims(img_array, axis=0)
-    
-#     # Get predictions
-#     predictions = model.predict(img_array)[0]
-    
-#     # Map index to class names
-#     class_labels = list(train_generator.class_indices.keys())
-    
-#     # Sort and print all confidence scores higher than 1%
-#     print("\n📊 --- Confidence Distribution ---")
-#     sorted_indices = np.argsort(predictions)[::-1]
-#     for idx in sorted_indices:
-#         confidence = predictions[idx] * 100
-#         if confidence > 1.0: # Only print relevant classes
-#             print(f"{class_labels[idx]}: {confidence:.2f}%")
-
-# # Usage:
-# # predict_plant_disease('path_to_your_google_downloaded_image.jpg', model, train_generator)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,55 @@
 import streamlit as st
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+CLASS_NAMES = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
 
-st.title("🎈 AI - Plant Disease Detection")
+st.set_page_config(page_title="Plant Disease Detector", page_icon="🌱", layout="centered")
+
+
+
+@st.cache_resource
+def load_model():
+    return tf.keras.models.load_model("model/plant_disease_model.h5",compile=False)
+
+with st.spinner("🧠 Loading AI Model into memory... Please wait..."):
+    model = load_model()
+
+
+st.title("🎈 AI - Plant Disease Detection") 
+
 
 st.write(
-    "Upload image and get information about the disease"
+    "Upload image of plant leaf"
 )
 
-image = st.file_uploader("Upload Image")
+
+
+uploaded_file = st.file_uploader("Choose a leaf image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image,"Uploaded Leaf Image", use_container_width=True)
+    
+    if st.button("🔍 Run AI Diagnosis"):
+        with st.spinner("Analyzing Image..."):
+            img  = image.resize((224,224))
+            if img.mode != "RGB":
+                img = img.convert('RGB')
+            img_array = np.array(img)/255.0
+
+            img_array = np.expand_dims(img_array , axis=0)
+            
+            prediction = model.predict(img_array,verbose=0)
+            prediction_index = np.argmax(prediction[0])
+            
+            confidence = prediction[0][prediction_index]*100
+            
+            raw_prediction = CLASS_NAMES[prediction_index]
+            
+            st.success(f"**Diagnosis:** {raw_prediction.replace("___",'-')} ")
+            st.metric(label="Confidence Score", value=f"{confidence:.2f}%")
+    
+
 
 
